64b6957aaa00d54b451101881cd7ebe1a0e82c3a
Aga K. <59511312+agnieszkaq@users.noreply.github.com>
2020-01-11 12:46:35 +0100

    Add JSP files, getting ready to create WEB app.

--- foodapplication_foodapplication/src/main/java/app/foodapplication/controller/RecipeController.java
+++ foodapplication_foodapplication/src/main/java/app/foodapplication/controller/RecipeController.java
@@ -2,6 +2,7 @@ package app.foodapplication.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,19 +11,26 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 import app.foodapplication.model.Recipe;
 import app.foodapplication.service.RecipeService;
 
-@RestController
-@RequestMapping("/api")
+@Controller
 public class RecipeController {
 
 	@Autowired
 	private RecipeService recipeService;
 
-	@GetMapping("/recipe")
-	public List<Recipe> get() {
-		return recipeService.get();
+//	@GetMapping("/recipe")
+//	public List<Recipe> get() {
+//		return recipeService.get();
+//	}
+
+	@RequestMapping(value = { "/", "home", "/index" })
+	public ModelAndView get() {
+		ModelAndView mav = new ModelAndView("recipeList");
+		return mav;
 	}
 
 	@GetMapping("/recipe/{id}")
